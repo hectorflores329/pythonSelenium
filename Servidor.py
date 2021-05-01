35aaf6e43dede5030ab991b1e491901d3b68ffa0
hectorflores329 <hectorflores329@gmail.com>
2021-04-30 22:00:21 -0600

    Commit 12

--- Servidor.py
+++ Servidor.py
@@ -7,9 +7,6 @@ def getDriver():
     options = Options()
     options.log.level = "trace"
     options.add_argument("--headless")
-    options.set_preference("browser.download.folderList", 2)
-    options.set_preference("browser.download.manager.showWhenStarting", False)
-    options.set_preference("browser.helperApps.neverAsk.saveToDisk", "text/csv") #, "application/json")
     driver = webdriver.Firefox(options=options)
     driver.set_page_load_timeout("60")
     driver.get("http://cognos.deis.cl/ibmcognos/cgi-bin/cognos.cgi?b_action=cognosViewer&ui.action=run&ui.object=/content/folder%5B@name=%27PUB%27%5D/folder%5B@name=%27REPORTES%27%5D/folder%5B@name=%27Atenciones%20de%20Urgencia%27%5D/report%5B@name=%27Atenciones%20Urgencia%20-%20Vista%20por%20semanas%20-%20Servicios%27%5D&ui.name=Atenciones%20Urgencia%20-%20Vista%20por%20semanas%20-%20Servicios&run.outputFormat=&run.prompt=true#")
