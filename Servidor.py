da060e1b46a172afcf87ff1d43c3c96cfd2c0502
hectorflores329 <hectorflores329@gmail.com>
2021-05-03 15:34:40 -0600

    Commit

--- Servidor.py
+++ Servidor.py
@@ -99,15 +99,15 @@ def descargarTablas():
                     yearsStable = stable.find_elements_by_tag_name("option")
 
                     tabla = driver.find_elements_by_class_name("xt")
-                    nombreArchivo = yearsValues[i].text + "_" + yearsRegion[j].text + "_"  + yearsStableType[k].text + "_" + yearsStable[l].text + ".csv"
+                    nombreArchivo = yearsValues[i].text + "_" + yearsRegion[j].text + "_"  + yearsStableType[k].text + "_" + yearsStable[l].text + ".xlsx"
 
                     try:
-                        dataF = {'col1': [1, 2], 'col2': [3, 4]}
+                        dataF = {'Columna 1': [1, 2], 'Columna 2': [3, 4]}
 
                         df = pd.DataFrame(data=dataF)
                         print(df)
                         df.to_excel(nombreArchivo, index=False)                      
-                        print("tablas/" + nombreArchivo)
+                        print(+ nombreArchivo)
 
                     except:
                         print("No se ha guardado la tabla.")
