f54b62f747d980249955e39af17d8f1671a2452f
hectorflores329 <hectorflores329@gmail.com>
2021-05-03 11:13:41 -0600

    Commit 9

--- Servidor.py
+++ Servidor.py
@@ -18,9 +18,10 @@ def saveCSV(tabla, anio, region, tipo_Establecimiento, nombre_establecimiento, n
     html = tabla[0].get_attribute('innerHTML')
     html = '<table> ' + html + ' </table>'
     html = html.replace(".","")
-    print(html)
     data = pd.read_html(html, skiprows=2)
+    print("UNOOOO 1111111111111" + data)
     df = data[0]
+    print("DOOOOS 2222222222222" + data[0])
     columnas = list(df.columns)
     columnas[0] = "Total"
     columnas.insert(0,"Urgencia")
